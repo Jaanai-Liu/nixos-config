48c4953d65497a72afce6b2fc45de6cb4733e9f0
Jaanai-Liu <liujaanai@gmail.com>
2026-04-10 20:27:48 +0800

    just openlist

--- vars/nixpkgs-config.nix
+++ vars/nixpkgs-config.nix
@@ -1,18 +1,22 @@
-_: {
+{
+  lib,
+  ...
+}:
+{
   allowUnfree = true;
 
   # permittedInsecurePackages = [
   #   "alist-3.45.0"
   # ];
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "alist-${pkgs.alist.version}"
-    "ventoy-${pkgs.ventoy.version}"
-  ];
+  # nixpkgs.config.permittedInsecurePackages = [
+  #   "alist-${pkgs.alist.version}"
+  #   "ventoy-${pkgs.ventoy.version}"
+  # ];
 
-  # nixpkgs.config.allowInsecurePredicate =
-  #   pkg:
-  #   builtins.elem (lib.getName pkg) [
-  #     "ventoy"
-  #   ];
+  allowInsecurePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "ventoy"
+    ];
 }
