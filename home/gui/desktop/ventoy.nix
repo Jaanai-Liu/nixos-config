88b1e4867cf323ed343572d39de69dbd3e8b09b9
Jaanai-Liu <liujaanai@gmail.com>
2026-04-10 19:17:40 +0800

    add ventoy allow

--- home/gui/desktop/ventoy.nix
+++ home/gui/desktop/ventoy.nix
@@ -6,4 +6,14 @@
   home.packages = with pkgs; [
     ventoy
   ];
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "ventoy-${pkgs.ventoy.version}"
+  ];
+
+  # nixpkgs.config.allowInsecurePredicate =
+  #   pkg:
+  #   builtins.elem (lib.getName pkg) [
+  #     "ventoy"
+  #   ];
 }
